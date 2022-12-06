133dc663e3be1743e4e6fcd6c551c248d6f3da0b
Bread <100310011+70839@users.noreply.github.com>
2022-12-06 12:40:45 -0500

    Update program.js

--- program.js
+++ program.js
@@ -1,5 +1,5 @@
 $("#search").submit(function(event){
   event.preventDefault();
   var input = $("#help").val();
-  window.open("https://www.google.com/search?q="+input, name="_blank");
+  window.open("https://www.google.com/search?q="+input+"+site%3Asouthernhanger.net", name="_blank");
 });

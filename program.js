1e559ce04104197a1e0e879d823ab5b70c08e421
Bread <100310011+70839@users.noreply.github.com>
2022-12-06 12:38:56 -0500

    Update program.js

--- program.js
+++ program.js
@@ -1,5 +1,5 @@
 $("#search").submit(function(event){
   event.preventDefault();
   var input = $("#help").val();
-  window.open("https://www.google.com/search?q="+input+"&surl=1&safe=active&ssui=on", name="_blank");
+  window.open("https://www.google.com/search?q="+input, name="_blank");
 });

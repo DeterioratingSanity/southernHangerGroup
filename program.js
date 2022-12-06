d86bcd1b2e42031830562d4c04221927acb574c8
Bread <100310011+70839@users.noreply.github.com>
2022-12-06 13:11:27 -0500

    Update program.js

--- program.js
+++ program.js
@@ -1,8 +1,9 @@
-$("#search button").click(function(){
+$("#search button:first-child").click(function(){
   $("#search").submit();
 }  
 $("#search").submit(function(event){
   event.preventDefault();
   var input = $("#help").val();
   $("#gSearch").attr("src", "https://www.google.com/search?q="+input+"+site%3Asouthernhanger.net");
+  $("#search button:nth-child(2)").click();
 });

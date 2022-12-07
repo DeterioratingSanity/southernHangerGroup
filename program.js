0bb10fbc4f1b9364e11aa11c137c07fae3ba24fb
Bread <100310011+70839@users.noreply.github.com>
2022-12-07 08:30:35 -0500

    Update program.js

--- program.js
+++ program.js
@@ -1,9 +1,9 @@
 $("#search button").click(function(){
   var input = $("#help").val();
-  $("#gSearch").attr("src", "http://www.google.com/custom?q="+input+"+site%3Asouthernhanger.net&btnG=Search");
+  $("#gSearch").attr("src", "https://www.google.com/custom?q="+input+"+site%3Asouthernhanger.net&btnG=Search");
 });
 $("#search").submit(function(event){
   event.preventDefault();
   var input = $("#help").val();
-  $("#gSearch").attr("src", "http://www.google.com/custom?q="+input+"+site%3Asouthernhanger.net&btnG=Search");
+  $("#gSearch").attr("src", "https://www.google.com/custom?q="+input+"+site%3Asouthernhanger.net&btnG=Search");
 });

9e94613706c9a81d9cb63561242f9acd4c378c8f
Bread <100310011+70839@users.noreply.github.com>
2022-12-07 08:56:32 -0500

    Update program.js

--- program.js
+++ program.js
@@ -1,16 +1,1 @@
-/*Code made by gotoplanb*/
-var googleSearchIframeName = "cse-search-results";
-var googleSearchFormName = "cse-search-box";
-var googleSearchFrameWidth = 600;
-var googleSearchDomain = "www.google.com";
-var googleSearchPath = "/cse";
-/*Ahrens code*/
-$("#search button").click(function(){
-  var input = $("#help").val();
-  $("#gSearch").attr("src", "https://www.google.com/custom?q="+input+"+site%3Asouthernhanger.net&btnG=Search");
-});
-$("#search").submit(function(event){
-  event.preventDefault();
-  var input = $("#help").val();
-  $("#gSearch").attr("src", "https://www.google.com/custom?q="+input+"+site%3Asouthernhanger.net&btnG=Search");
-});
+//

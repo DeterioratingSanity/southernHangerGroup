b82f5812e6566ae7c661e52797f690536511109f
Bread <100310011+70839@users.noreply.github.com>
2022-12-07 08:46:36 -0500

    Update program.js

--- program.js
+++ program.js
@@ -1,3 +1,10 @@
+/*Code made by gotoplanb*/
+var googleSearchIframeName = "cse-search-results";
+var googleSearchFormName = "cse-search-box";
+var googleSearchFrameWidth = 600;
+var googleSearchDomain = "www.google.com";
+var googleSearchPath = "/cse";
+/*Ahrens code*/
 $("#search button").click(function(){
   var input = $("#help").val();
   $("#gSearch").attr("src", "https://www.google.com/custom?q="+input+"+site%3Asouthernhanger.net&btnG=Search");
